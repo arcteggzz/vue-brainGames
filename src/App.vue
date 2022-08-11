e3d0ce5cef418e8c10721902a473d5e39626100f
Oghenetega Esedere <arcteggzz@gmail.com>
2022-08-11 17:48:00 +0100

    Initial Setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    Vuex
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+  components: {}
+});
+</script>
+
+<style>
+</style>
